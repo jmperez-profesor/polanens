5c26adc65cc0ae6815e13fbd92bfb77b11eea339
Jose Manuel Pérez Torres <jm.pereztorres@edu.gva.es>
2026-08-31 19:53:21 +0200

    Cambios nuevos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "reparto-volley-v1";
+const CACHE_NAME = "reparto-volley-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,18 +6,22 @@ const ASSETS = [
   "./js/app.js",
   "./js/db.js",
   "./js/seed.js",
+  "./js/config.js",
+  "./js/supabase-client.js",
   "./manifest.webmanifest",
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
